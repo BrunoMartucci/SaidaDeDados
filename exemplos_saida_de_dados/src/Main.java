9f08f6570d3d9695e9afa8db4e3b50dfef9b4bb1
Bruno Martucci <brunomartucci77@gmail.com>
2023-08-14 14:11:31 -0300

    first commit

---
+++ exemplos_saida_de_dados/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println(product1 + " which price is $ " + price1);
+		System.out.println(product2 + " whice price is $ " + price2);
+		System.out.println("Record: " + age + " years old, code " + code + " and gender: " + gender);
+		System.out.println("Measue with eight decimal places: " + measure);
+		System.out.printf("Rouded (three decimal places) %.3f %n ", measure);
+		System.out.printf("US decimal point: %.3f %n ", measure);
+	}
+	
+	
+}
